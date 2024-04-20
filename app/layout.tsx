419b6092b847792e450a66de68e7e303749ec8a1
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-04-20 21:07:06 +0000

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+///import the font style from the created font ui
+import {inter} from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
